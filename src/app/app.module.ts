20d2c31d4b1da4488efb1ed3956fd76b0036f504
Pawel Winek <pwinek@gmail.com>
2020-10-30 14:52:23 +0100

    add bluepizza

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products.ts
@@ -0,0 +1,64 @@
+
+
+export const products = [
+  {
+    identifier: '1',
+    name: 'Pizza Margeritha',
+    price: 4.50,
+    type: 'pizza'
+  },
+  {
+    identifier: '2',
+    name: 'Pizza Salami',
+    price: 5.50,
+    type: 'pizza'
+  },
+  {
+    identifier: '3',
+    name: 'Lasagne',
+    price: 5.00,
+    type: 'pasta'
+  },
+  {
+    identifier: '4',
+    name: 'Pasta Napoli',
+    price: 5.50,
+    type: 'pasta'
+  },
+  {
+    identifier: '5',
+    name: 'Pasta Bolognese',
+    price: 6.00,
+    type: 'pasta'
+  },
+  {
+    identifier: '6',
+    name: 'Wiener Schnitzel',
+    price: 6.50,
+    type: 'schnitzel'
+  },
+  {
+    identifier: '7',
+    name: 'Zigeuner Schnitzel',
+    price: 7.50,
+    type: 'schnitzel'
+  },
+  {
+    identifier: '8',
+    name: 'Pommes klein',
+    price: 1.20,
+    type: 'snacks'
+  },
+  {
+    identifier: '9',
+    name: 'Pommes groß',
+    price: 2.00,
+    type: 'snacks'
+  },
+  {
+    identifier: '10',
+    name: 'Ben & Jerry\'s',
+    price: 2.00,
+    type: 'snacks'
+  }
+];
